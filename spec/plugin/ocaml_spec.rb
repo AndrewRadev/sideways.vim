9dd871ee10e0e553214d94cab246ad5107eeafc3
Andrew Radev <andrey.radev@gmail.com>
2018-12-26 14:02:49 +0200

    Extra ocaml test

--- spec/plugin/ocaml_spec.rb
+++ spec/plugin/ocaml_spec.rb
@@ -28,4 +28,17 @@ describe "ocaml lists" do
       let xs = [|1;3;2|]
     EOF
   end
+
+  specify "[|arrays with tuples|]" do
+    set_file_contents <<-EOF
+      let xs = [|1, 2; 3, 4|]
+    EOF
+
+    vim.search('2')
+    vim.right
+
+    assert_file_contents <<-EOF
+      let xs = [|3, 4; 1, 2|]
+    EOF
+  end
 end
